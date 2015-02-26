c2c1aa562ffe6f0aa8283348e9304f0cb11d8117
Andrei Trandafir <andrei.trandafir@endava.com>
2015-02-26 10:45:50 +0200

    configured TestNG plugin

--- src/test/java/com/sayem/AppTest.java
+++ src/test/java/com/sayem/AppTest.java
@@ -2,46 +2,13 @@ package com.sayem;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+public class AppTest{
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-	public void testApp()
-	{
+	@Test 
+	public void testRegister(){
 		WebDriver driver = new FirefoxDriver();
-		System.out.println("before creating the instance");
-
-		// Go to the Google Suggest home page
-		driver.get("http://www.google.com/webhp?complete=1&hl=en");
-		System.out.println("after creating the instance");
-		assertTrue( true );
+		driver.get("http://newtours.demoaut.com/");
 	}
 }
